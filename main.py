bdf81a4a3623e29a1a5aee682efc1053ac375e55
personalongithub <personalongithub@gmail.com>
2022-06-03 16:28:28 -0500

    test

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ async def help(ctx):
         title = 'Help',
         description = 'For more info about a command, use `$help [command]`',
     )    
-    embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name='Commands', value='`hello`, `help`, `roll`, `8ball`, `invite`')
+    #embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     await ctx.respond('', embed=embed)
 
 @bot.slash_command(description='Roll a die!')
@@ -32,8 +32,8 @@ async def c_eight_ball(ctx:discord.ApplicationContext, question:str):
         color=0x3abad3,
         title='Magic 8-Ball :8ball:',
     )
-    embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name=question, value=eight_ball.eight_ball(question))
+    #embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     await ctx.respond('', embed=embed)
 
 @bot.slash_command(name='invite', description='Invite the bot to a server!')
@@ -42,7 +42,7 @@ async def invite(ctx):
         color=0x3abad3,
         title='Invite the bot!',
     )
-    embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
+    #embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name='Invite Personal to a server', value='**[Personal Invite Link](https://discord.com/api/oauth2/authorize?client_id=954564559467868171&permissions=274877908992&scope=bot%20applications.commands)**')
     await ctx.respond('', embed=embed)
 
