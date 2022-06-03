caf098c1e2f97e9f65cda0f3fcc00aa8f5055e0b
personalongithub <personalongithub@gmail.com>
2022-06-03 16:31:46 -0500

    test

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ async def help(ctx):
     embed = discord.Embed(
         color = 0x3abad3,
         title = 'Help',
-        description = 'For more info about a command, use `$help [command]`',
+        description = 'For more info about a command, use `/help [command]`',
     )    
     embed.add_field(name='Commands', value='`hello`, `help`, `roll`, `8ball`, `invite`')
-    #embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
-    await ctx.respond('', embed=embed)
+    embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
+    await ctx.respond(embed=embed)
 
 @bot.slash_command(description='Roll a die!')
 @discord.option('number', description='Choose a number!', min_value=2, default=6)
@@ -33,8 +33,8 @@ async def c_eight_ball(ctx:discord.ApplicationContext, question:str):
         title='Magic 8-Ball :8ball:',
     )
     embed.add_field(name=question, value=eight_ball.eight_ball(question))
-    #embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
-    await ctx.respond('', embed=embed)
+    embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
+    await ctx.respond(embed=embed)
 
 @bot.slash_command(name='invite', description='Invite the bot to a server!')
 async def invite(ctx):
@@ -42,9 +42,9 @@ async def invite(ctx):
         color=0x3abad3,
         title='Invite the bot!',
     )
-    #embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name='Invite Personal to a server', value='**[Personal Invite Link](https://discord.com/api/oauth2/authorize?client_id=954564559467868171&permissions=274877908992&scope=bot%20applications.commands)**')
-    await ctx.respond('', embed=embed)
+    embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
+    await ctx.respond(embed=embed)
 
 @bot.slash_command(description='Test the ping of the bot!')
 async def ping(ctx):
