cf3816e6f0fe0062120eb71548e4b8557b6e69f2
personalongithub <personalongithub@gmail.com>
2022-06-03 14:08:35 -0500

    test

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ bot = discord.Bot()
 async def on_ready(): 
     print(f'We have logged in as {bot.user}')
     
-@bot.slash_command(name='hello', description='Says hello!')
+@bot.slash_command(description='Says hello!')
 async def hello(ctx):
     await ctx.respond(f'Hello, {ctx.author.name}!')
 
-@bot.slash_command()
+@bot.slash_command(description='Shows help options for commands')
 async def help(ctx):
     embed = discord.Embed(
         title = 'Help',
         description = 'For more info about a command, use `$help [command]`',
-        color = discord.Color.random(),
+        color = discord.Color.green(),
     )
     embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name='Commands', value='`hello`, `help`, `roll`, `8ball`, `invite`')
     await ctx.respond(embed=embed)
 
-@bot.slash_command(name = 'roll', description = 'Roll a die!')
+@bot.slash_command(description='Roll a die!')
 @discord.option('number', description='Choose a number!', min_value=2, default=6)
-async def slash_roll(ctx:discord.ApplicationContext, number:int):
+async def roll(ctx:discord.ApplicationContext, number:int):
     await ctx.respond(f'**{ctx.author.name}** rolled a **{random.randint(1, number)}**! (1-{number})')
 
-@bot.slash_command(name = '8ball', description = "What's your fortune?")
+@bot.slash_command(name='8ball', description="What's your fortune?")
 @discord.option('question', description='Ask your question!')
-async def slash_eight_ball(ctx:discord.ApplicationContext, question:str):
+async def c_eight_ball(ctx:discord.ApplicationContext, question:str):
     embed = discord.Embed(
-        title = 'Magic 8-Ball :8ball:',
-        color = discord.Color.random(),
+        title='Magic 8-Ball :8ball:',
+        color=discord.Color.green(),
     )
     embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name=question, value=eight_ball.eight_ball(question))
@@ -39,8 +39,8 @@ async def slash_eight_ball(ctx:discord.ApplicationContext, question:str):
 @bot.slash_command(name='invite', description='Invite the bot to a server!')
 async def invite(ctx):
     embed = discord.Embed(
-        title = 'Invite the bot!',
-        color = discord.Color.random(),
+        title='Invite the bot!',
+        color=discord.Color.green(),
     )
     embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name='Invite Personal to a server', value='**[Personal Invite Link](https://discord.com/api/oauth2/authorize?client_id=954564559467868171&permissions=274877908992&scope=bot%20applications.commands)**')
