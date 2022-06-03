6402f541114bc9d66c2af80a4783af3619447f6a
personalongithub <personalongithub@gmail.com>
2022-06-03 16:19:39 -0500

    color test

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def help(ctx):
     embed = discord.Embed(
         title = 'Help',
         description = 'For more info about a command, use `$help [command]`',
-        color = discord.Color.green(),
+        color = 0x3abad3,
     )
     embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name='Commands', value='`hello`, `help`, `roll`, `8ball`, `invite`')
@@ -30,7 +30,7 @@ async def roll(ctx:discord.ApplicationContext, number:int):
 async def c_eight_ball(ctx:discord.ApplicationContext, question:str):
     embed = discord.Embed(
         title='Magic 8-Ball :8ball:',
-        color=discord.Color.green(),
+        color=0x3abad3,
     )
     embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name=question, value=eight_ball.eight_ball(question))
@@ -40,7 +40,7 @@ async def c_eight_ball(ctx:discord.ApplicationContext, question:str):
 async def invite(ctx):
     embed = discord.Embed(
         title='Invite the bot!',
-        color=discord.Color.green(),
+        color=0x3abad3,
     )
     embed.set_footer(text = f'Requested by {ctx.author}', icon_url = ctx.author.avatar_url)
     embed.add_field(name='Invite Personal to a server', value='**[Personal Invite Link](https://discord.com/api/oauth2/authorize?client_id=954564559467868171&permissions=274877908992&scope=bot%20applications.commands)**')
