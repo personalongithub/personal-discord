877d914ba2ef5294bde21f8a6bb4bf8222ee06fb
personalongithub <personalongithub@gmail.com>
2022-06-22 21:46:10 -0500

    adding maximum value for stringify

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ async def coordinate(ctx: discord.ApplicationContext, location: str, privacy: bo
             ephemeral=bool(privacy))
 
 @bot.slash_command(description='Make a random string of characters of any length')
-@discord.option('length', description='How long will your string be?', min_value=1)
+@discord.option('length', description='How long will your string be?', min_value=1, max_value=2048)
 async def stringify(ctx: discord.ApplicationContext, length: int):
     """Makes a random string that is a select amount of characters."""
     string = ''
